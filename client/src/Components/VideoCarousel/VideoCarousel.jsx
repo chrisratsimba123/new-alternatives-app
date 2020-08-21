29b2a8f8739822e3b91f9072631a9601524ab459
Daynamarie <Dayna.christison@gmail.com>
2020-08-21 16:06:05 -0400

    added in react and a return placeholder

---
+++ client/src/Components/VideoCarousel/VideoCarousel.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VideoCarousel = () => {
+
+    return(
+        <div>
+            <h2>This is the Video Carsousel</h2>
+        </div>
+    )
+}
+
+export default VideoCarousel
