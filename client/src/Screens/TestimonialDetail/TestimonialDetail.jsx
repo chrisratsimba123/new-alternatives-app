c6542f3fed28dfbc58923be7eea69af7a5c3390b
Daynamarie <Dayna.christison@gmail.com>
2020-08-21 15:34:44 -0400

    centered - added edit and delete buttons

--- client/src/Screens/TestimonialDetail/TestimonialDetail.jsx
+++ client/src/Screens/TestimonialDetail/TestimonialDetail.jsx
@@ -8,6 +8,10 @@ const TestimonialDetail = () => {
             <div className="testimonial">
                 <div className="content">This is my testimonial to new alternatives</div>
                 <div className="author">- This is who wrote it</div>
+                <div className="button-container">
+                    <button className="button-edit">Edit</button>
+                    <button className="button-delete">Delete</button>
+                </div>
             </div>
         </div>
 
