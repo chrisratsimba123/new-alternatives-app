5c486be99eb8a54316de5151017b1f5b22fb655e
ramizackaryshamir <ramizackshamir@icloud.com>
2020-08-21 17:10:59 -0400

    added h1 Component/Section names

--- client/src/Screens/Home/Home.jsx
+++ client/src/Screens/Home/Home.jsx
@@ -3,8 +3,38 @@ import './Home.css'
 
 const Home = () => {
 
-    return(
-        <h1>Home Page</h1>
+  return (
+    <>
+      
+      <div className='nav'>
+          <h1>Nav</h1>
+      </div>
+      
+      <div className='hero'>
+          <h1>Hero</h1>
+      </div>
+
+      <div className='purple-icons-first'>
+          <h1>Three Purple Icons</h1> 
+      </div>
+
+      <div className='who-we-help'>
+        <h1>Who We Help</h1>
+      </div>
+
+      <div className='carousel-we-exist-to'>
+        <h1>We Exist To Carousel</h1>
+      </div>
+
+      <div className='get-involved'>
+        <h1>Get Involved</h1>
+      </div>
+
+      <div className='footer'>
+        <h1>Footer</h1>
+      </div>
+      
+    </>
     )
 }
 
