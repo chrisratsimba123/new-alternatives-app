4a338114498b2e86f588db1131d2e7d51d97afdc
Daynamarie <Dayna.christison@gmail.com>
2020-08-22 10:46:11 -0400

    getting the testimonials as a value in edit box

--- client/src/Screens/TestimonialDetail/TestimonialDetail.jsx
+++ client/src/Screens/TestimonialDetail/TestimonialDetail.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 import './TestimonialDetail.css'
-import { getTestimonial, updateTestimonial, deleteTestimonial } from '../../Services/Testimonials'
+import { getTestimonial } from '../../Services/Testimonials'
 
 const TestimonialDetail = () => {
 
@@ -10,7 +10,6 @@ const TestimonialDetail = () => {
         content: '',
         author: '',
     })
-    /* const [updated, setUpdated] = useState(false) */
     let { id } = useParams()
 
     useEffect(() => {
@@ -22,30 +21,38 @@ const TestimonialDetail = () => {
     }, [id])
 
     const handleChange = (e) => {
-        const { key, value } = e.target.value
+        const { key, value } = e.target
         setTestimonial({
             ...testimonial, 
             [key]: value
         })
-
     }
 
     return (
         <div className="testimonial-detail">
             <div className="testimonial">
-                <textarea 
-                    className="content"
-                    value={testimonial.content}
-                    key = 'content'
-                    required
-                    onChange={handleChange}
-                    placeholder="This is my testimonial to new alternatives"
-                />
-                <div className="author">- This is who wrote it</div>
-                <div className="button-container">
-                    <button className="button-edit">Edit</button>
-                    <button className="button-delete">Delete</button>
-                </div>
+                <form>
+                    <textarea 
+                        className="edit-content"
+                        value={testimonial.content}
+                        key='content'
+                        required
+                        onChange={handleChange}
+                        placeholder="This is my testimonial to new alternatives"
+                    />
+                    <input 
+                        className="edit-author"
+                        value={testimonial.author}
+                        key="author"
+                        required
+                        onChange={handleChange}
+                        placeholder="- This is who wrote it"
+                    />
+                    <div className="button-container">
+                        <button className="button-edit">Edit</button>
+                        <button className="button-delete">Delete</button>
+                    </div>
+                </form>
             </div>
         </div>
 
