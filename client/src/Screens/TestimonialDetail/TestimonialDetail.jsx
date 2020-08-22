b51103db3f17cbe08a5aa65299692e277c8165ac
Daynamarie <Dayna.christison@gmail.com>
2020-08-22 10:02:19 -0400

    handle change for testimonial content

--- client/src/Screens/TestimonialDetail/TestimonialDetail.jsx
+++ client/src/Screens/TestimonialDetail/TestimonialDetail.jsx
@@ -2,16 +2,45 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 import './TestimonialDetail.css'
-/* import { getTestimonial, updateTestimonial, deleteTestimonial } from '../Services/Testimonials' */
+import { getTestimonial, updateTestimonial, deleteTestimonial } from '../../Services/Testimonials'
 
 const TestimonialDetail = () => {
 
-/*     const [content, setContent] =  */
+    const [testimonial, setTestimonial] =  useState({ 
+        content: '',
+        author: '',
+    })
+    /* const [updated, setUpdated] = useState(false) */
+    let { id } = useParams()
+
+    useEffect(() => {
+        const grabTestimonial = async () => {
+            const testimonial = await getTestimonial(id)
+            setTestimonial(testimonial)
+        }
+        grabTestimonial()
+    }, [id])
+
+    const handleChange = (e) => {
+        const { key, value } = e.target.value
+        setTestimonial({
+            ...testimonial, 
+            [key]: value
+        })
+
+    }
 
     return (
         <div className="testimonial-detail">
             <div className="testimonial">
-                <div className="content">This is my testimonial to new alternatives</div>
+                <textarea 
+                    className="content"
+                    value={testimonial.content}
+                    key = 'content'
+                    required
+                    onChange={handleChange}
+                    placeholder="This is my testimonial to new alternatives"
+                />
                 <div className="author">- This is who wrote it</div>
                 <div className="button-container">
                     <button className="button-edit">Edit</button>
