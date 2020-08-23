1c7a7f5b745b3c00c5d7c0577c97ed04e99e798b
ramizackaryshamir <ramizackshamir@icloud.com>
2020-08-23 13:27:32 -0400

    Layout for Home Screen/ Footer and Carousel Interior Styling on hold until Monday 08/24/2020

--- client/src/Screens/Home/Home.jsx
+++ client/src/Screens/Home/Home.jsx
@@ -8,27 +8,62 @@ const Home = () => {
       <div className='layout'>
         
         <div className='nav-component'>
+
           <div className='title' alt='new alternatives'>
-            <h2>New<br />Alternatives<br />(title)</h2>
+            <h4>New<br />Alternatives<br />(title)</h4>
           </div>
-          {/*<h1>Nav</h1>*/}
-          <div className='social-component'>
-            <div className='social-icon' alt='facebook'>
+
+          <div className='nav-bar'>
+            <div className='nav-menu' alt='navigation menu'>
+              <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Our Stories</li>
+                <li>Get Help</li>
+                <li>Get Involved</li>
+              </ul>
+             
             </div>
-            <div className='social-icon' alt='twitter'>
+            <div className='social-component'>
+              <div className='social-icon' alt='facebook'>
+              </div>
+              <div className='social-icon' alt='twitter'>
+              </div>
+              <div className='social-icon' alt='instagram'>
+              </div>
             </div>
-            <div className='social-icon' alt='instagram'>
-            </div>
+            
           </div>
+          
           <button className='donate' alt='donate button'>Donate</button>
         </div>
       
         <div className='hero'>
-          <h1>Hero</h1>
+          <div className='shadow-box'>
+            <h2>New Alternatives for LGBTQ+ Homeless Youth</h2>
+            <p>New Alternatives increases the self-sufficiency of LGBTQ+ homeless youth by enabling them to transition out of the shelter system to stable adult lives.</p>
+          </div>
         </div>
 
         <div className='purple-icons-first'>
-          <h1>Three Purple Icons</h1> 
+          <div className='icon'>
+            <div className='icon-text'>
+              <h1>$500K</h1>
+              <p>raised in one year</p>
+            </div>
+          </div>
+          <div className='icon'>
+            <div className='icon-text'>
+              <h1>40%</h1>
+              <p>of LGBTQ+ are<br/>homeless in NYC</p>
+            </div>
+          </div>
+          <div className='icon'>
+            <div className='icon-text'>
+              <h1>10K</h1>
+              <p>meals provided thus far</p>
+            </div>
+          </div>
         </div>
 
         <div className='who-we-help'>
@@ -43,6 +78,15 @@ const Home = () => {
 
         <div className='get-involved'>
           <h1>Get Involved</h1>
+          <div className='get-involved-icon'>
+            
+          </div>
+          <div className='get-involved-text'>
+          <h2>Get Help</h2>
+          <h2>Volunteer</h2>
+          <h2>Donate</h2>
+          </div> 
+          
         </div>
 
         <div className='footer'>
