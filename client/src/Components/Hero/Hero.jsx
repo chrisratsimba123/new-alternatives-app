08d63c6c4e01872a659b18e6989a9a6827a43275
ramizackaryshamir <ramizackshamir@icloud.com>
2020-08-24 14:12:54 -0400

    all CSS abstractions are in this branch

---
+++ client/src/Components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div>
+      <div className='hero'>
+          <div className='shadow-box'>
+            <h2>New Alternatives for LGBTQ+ Homeless Youth</h2>
+            <p>New Alternatives increases the self-sufficiency of LGBTQ+ homeless youth by enabling them to transition out of the shelter system to stable adult lives.</p>
+          </div>
+        </div>
+    </div>
+  )
+}
--- client/src/Screens/Home/Home.jsx
+++ client/src/Screens/Home/Home.jsx
@@ -7,43 +7,12 @@ const Home = () => {
     <>
       <div className='layout'>
         
-        <div className='nav-component'>
+       
 
-          <div className='title' alt='new alternatives'>
-            <h4>New<br />Alternatives<br />(title)</h4>
-          </div>
-
-          <div className='nav-bar'>
-            <div className='nav-menu' alt='navigation menu'>
-              <ul>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Our Stories</li>
-                <li>Get Help</li>
-                <li>Get Involved</li>
-              </ul>
-             
-            </div>
-            <div className='social-component'>
-              <div className='social-icon' alt='facebook'>
-              </div>
-              <div className='social-icon' alt='twitter'>
-              </div>
-              <div className='social-icon' alt='instagram'>
-              </div>
-            </div>
-            
-          </div>
+         
           
-          <button className='donate' alt='donate button'>Donate</button>
-        </div>
+         
       
-        <div className='hero'>
-          <div className='shadow-box'>
-            <h2>New Alternatives for LGBTQ+ Homeless Youth</h2>
-            <p>New Alternatives increases the self-sufficiency of LGBTQ+ homeless youth by enabling them to transition out of the shelter system to stable adult lives.</p>
-          </div>
-        </div>
 
         <div className='purple-icons-first'>
           <div className='icon'>
@@ -71,9 +40,7 @@ const Home = () => {
           <p className='who-we-help-p'>We assist LGBTQ+ youth and young adults who are 16-30 years old and homeless or at risk of homelessness. We define homelessness broadly to include living on the street, in a shelter, in a transitional living program, or lacking permanent housing.</p>
         </div>
 
-        <div className='carousel-we-exist-to'>
-          <h1>We Exist To Carousel DO NOT STYLE</h1>
-        </div>
+       
 
         <div className='get-involved'>
           <h1>Get Involved</h1>
@@ -88,9 +55,7 @@ const Home = () => {
           
         </div>
 
-        <div className='footer'>
-          <h1>Footer DO NOT STYLE</h1>
-        </div>
+        
           
     </div>
     </>
