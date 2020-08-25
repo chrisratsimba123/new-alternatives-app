a325a8e10d1d3af3ccc997dd29dd2f30bdbc6101
Daynamarie <Dayna.christison@gmail.com>
2020-08-25 17:26:48 -0400

    videos showing

--- client/src/Components/VideoCarousel/VideoCarousel.jsx
+++ client/src/Components/VideoCarousel/VideoCarousel.jsx
@@ -1,40 +1,60 @@
 import React from 'react';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
-import ReactPlayer from 'react-player'
+/* import ReactPlayer from 'react-player' */
 
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import "./VideoCarousel.css"
 import LeftArrow from "../../Assets/left-arrow.png";
 import RightArrow from "../../Assets/right-arrow.png";
-import Video from "../Videos/Purple.mp4"
+import Purple from "../../Assets/Videos/Purple.mp4"
+import Blue from "../../Assets/Videos/Blue.mp4"
+import Green from "../../Assets/Videos/Green.mp4"
+import Red from "../../Assets/Videos/Red.mp4"
+import Yellow from "../../Assets/Videos/Yellow.mp4"
 
 const VideoCarousel = () => {
-
+    
     return(    
         <div className="video-wrapper">                                             
             <CarouselProvider
+                className="carousel-complete"
                 naturalSlideWidth={400}
                 naturalSlideHeight={200}
-                totalSlides={3}
+                /* isintrinsicHeight={true} */ 
+                totalSlides={5}
+                visibleSlides={3}
             >
-                <ButtonBack className="video-back-btn">
+                <ButtonBack className="video-back-btn btn">
                     <img className="back-arrow" src={LeftArrow} />
                 </ButtonBack>          
                 <Slider className="video-container">
-                    <Slide index={0}>
-                        I am the first Slide.
-                        <ReactPlayer
-                            className='video'
-                            url= 'https://www.instagram.com/p/By2Wq89BeuT/'
-                            width='100%'
-                            height='100%'
-                            controls = {true}
-                        /> 
+                    <Slide>
+                        <video width="100%" height="100%" controls >
+                            <source src={Blue} type="video/mp4"/>
+                        </video>
                     </Slide>
-                    <Slide index={1}>I am the second Slide.</Slide>
-                    <Slide index={2}>I am the third Slide.</Slide>
+                    <Slide>
+                        <video width="100%" height="100%" controls >
+                            <source src={Green} type="video/mp4"/>
+                        </video>
+                    </Slide>
+                    <Slide>
+                        <video width="100%" height="100%" controls >
+                            <source src={Red} type="video/mp4"/>
+                        </video>
+                    </Slide>
+                    <Slide>
+                        <video width="100%" height="100%" controls >
+                            <source src={Yellow} type="video/mp4"/>
+                        </video>
+                    </Slide>
+                    <Slide>
+                        <video width="100%" height="100%" controls >
+                            <source src={Purple} type="video/mp4"/>
+                        </video>
+                    </Slide>
                 </Slider>
-                <ButtonNext className="video-forward-btn">
+                <ButtonNext className="video-forward-btn btn">
                     <img className="forward-arrow" src={RightArrow} alt="right arrow" />
                 </ButtonNext>
             </CarouselProvider>
@@ -46,10 +66,4 @@ export default VideoCarousel
 
 /* CITED 
 //https://github.com/express-labs/pure-react-carousel 
-
-
-
-
-                        <video width="750" height="500" controls >
-                            <source src={Video} type="video/mp4"/>
-                        </video>*/
+*/
