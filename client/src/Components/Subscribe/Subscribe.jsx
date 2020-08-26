14b3734ba21b73be6766b5c78d471a46f80ecc12
Chris Ratsimbazafy <ratsimbazafy@chriss-air.home>
2020-08-26 11:36:36 -0400

    changes to subscribe component + deadlinks new image

--- client/src/Components/Subscribe/Subscribe.jsx
+++ client/src/Components/Subscribe/Subscribe.jsx
@@ -4,16 +4,16 @@ import './Subscribe.css'
 
 const Subscribe = () => {
 
-    let subscribeButton = document.querySelector('subscribe-button')
-    subscribeButton.addEventListener('click', (e) => {
-        e.preventDefault()
-        let savedEmails = JSON.parse(window.localStorage.getItem('email'))
-        if(savedEmails === null) {
-            savedEmails = []
-        }
-        // savedEmails.push(sampleDeveloper) Push text input into savedEmails Array
-        window.localStorage.setItem('email', JSON.stringify(savedEmails))
-    })
+    // let subscribeButton = document.querySelector('subscribe-button')
+    // subscribeButton.addEventListener('click', (e) => {
+    //     e.preventDefault()
+    //     let savedEmails = JSON.parse(window.localStorage.getItem('email'))
+    //     if(savedEmails === null) {
+    //         savedEmails = []
+    //     }
+    //     // savedEmails.push(sampleDeveloper) Push text input into savedEmails Array
+    //     window.localStorage.setItem('email', JSON.stringify(savedEmails))
+    // })
 
     return(
     <>
