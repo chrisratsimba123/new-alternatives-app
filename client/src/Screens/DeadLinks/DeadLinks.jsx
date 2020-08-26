ee1f54e5ca4d7a7e6f0c6b3859ab5313325de3f5
ramizackaryshamir <ramizackshamir@icloud.com>
2020-08-26 17:30:21 -0400

    DeadLinks Component Styled

--- client/src/Screens/DeadLinks/DeadLinks.jsx
+++ client/src/Screens/DeadLinks/DeadLinks.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Layout from '../../Components/Shared/Layout/Layout'
 import Construction from '../../Assets/page-under-construction.png'
+import './DeadLinks.css'
 
 const DeadLinks = () => {
 
     return(
     <>
     <Layout>
-        <div className="-dead-image-div">
+        <div className="dead-image-div">
             {/* <h1 className="coming-soon">Coming Soon</h1> */}
         <img className="construction-image" src={Construction}/>
         </div>
