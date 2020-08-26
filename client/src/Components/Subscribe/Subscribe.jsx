3e04a4caf8c76a5433093f8dd4efc3057f7ebf73
Chris Ratsimbazafy <ratsimbazafy@chriss-air.home>
2020-08-26 09:01:15 -0400

    subscribe button skeleton + pseudocode

--- client/src/Components/Subscribe/Subscribe.jsx
+++ client/src/Components/Subscribe/Subscribe.jsx
@@ -4,6 +4,17 @@ import './Subscribe.css'
 
 const Subscribe = () => {
 
+    let subscribeButton = document.querySelector('subscribe-button')
+    subscribeButton.addEventListener('click', (e) => {
+        e.preventDefault()
+        let savedEmails = JSON.parse(window.localStorage.getItem('email'))
+        if(savedEmails === null) {
+            savedEmails = []
+        }
+        // savedEmails.push(sampleDeveloper) Push text input into savedEmails Array
+        window.localStorage.setItem('email', JSON.stringify(savedEmails))
+    })
+
     return(
     <>
     <form>
