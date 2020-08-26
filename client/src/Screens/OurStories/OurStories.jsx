8fd5afdc800f4d87c544f48c2f87dec8c321a1cb
donnywarren9 <donnywarren9@gmail.com>
2020-08-25 21:25:34 -0400

    our stories hero image complete

--- client/src/Screens/OurStories/OurStories.jsx
+++ client/src/Screens/OurStories/OurStories.jsx
@@ -4,6 +4,7 @@ import TestimonialsCarousel from "../../Components/TestimonialsCarousel/Testimon
 import { getTestimonials } from "../../Services/Testimonials";
 import Layout from "../../Components/Shared/Layout/Layout";
 import PhotoCarousel from "../../Components/PhotoCarousel/PhotoCarousel";
+import "./OurStories.css";
 
 const OurStories = () => {
   const [testimonials, updateTestimonials] = useState([]);
@@ -19,6 +20,16 @@ const OurStories = () => {
   return (
     <Layout>
       <div className="complete-our-stories">
+        <div className="our-stories-hero-container">
+          <div className="our-stories-shadow-box">
+            <h2>Our Stories</h2>
+            <p>
+              We all have a story... We all deserve the right to a happy ever
+              after. New Alternatives is here to make sure the LGBTQ+ youth have
+              the tools they need to create their own success stories.
+            </p>
+          </div>
+        </div>
         <TestimonialsCarousel content={testimonials} />
         <PhotoCarousel />
       </div>
