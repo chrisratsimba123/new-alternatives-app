64f1fe361d5ad3aeef84de862de1fdfa8fae85d3
Chris Ratsimbazafy <ratsimbazafy@chriss-air.home>
2020-08-26 14:57:22 -0400

    changed to Construction Image on Dead Links pages

--- client/src/Screens/DeadLinks/DeadLinks.jsx
+++ client/src/Screens/DeadLinks/DeadLinks.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Layout from '../../Components/Shared/Layout/Layout'
+import Construction from '../../Assets/page-under-construction.png'
 
 const DeadLinks = () => {
 
     return(
     <>
     <Layout>
-        <h1 className="coming-soon">Coming Soon</h1>
+        {/* <h1 className="coming-soon">Coming Soon</h1> */}
+        <img className="construction-image" src={Construction}/>
     </Layout>
     </>
     )
