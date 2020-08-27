30ad2f2c571e6868b0f076c07ee3c1569f5fd30c
Daynamarie <Dayna.christison@gmail.com>
2020-08-27 15:37:46 -0400

    added quotes to details and took away outline on buttons

--- client/src/Screens/TestimonialDetail/TestimonialDetail.jsx
+++ client/src/Screens/TestimonialDetail/TestimonialDetail.jsx
@@ -1,83 +1,96 @@
-import React, { useState, useEffect } from 'react'
-import { useParams, Redirect } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { useParams, Redirect } from "react-router-dom";
 
-import './TestimonialDetail.css'
-import '../../Components/Shared/Nav/Nav.css'
-import { getTestimonial, updateTestimonial, deleteTestimonial } from '../../Services/Testimonials'
-import Layout from '../../Components/Shared/Layout/Layout'
+import "./TestimonialDetail.css";
+import "../../Components/Shared/Nav/Nav.css";
+import {
+  getTestimonial,
+  updateTestimonial,
+  deleteTestimonial,
+} from "../../Services/Testimonials";
+import Layout from "../../Components/Shared/Layout/Layout";
+import LeftQuote from "../../Assets/left-quote.png";
+import RightQuote from "../../Assets/right-quote.png";
 
 const TestimonialDetail = () => {
-    useEffect(() => {
-        window.scrollTo(0,0);
-    }, [])
-    const [testimonial, setTestimonial] =  useState({ 
-        content: "",
-        author: "",
-    });
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  const [testimonial, setTestimonial] = useState({
+    content: "",
+    author: "",
+  });
 
-    const [updated, setUpdated] = useState(false);
-    let { id } = useParams();
+  const [updated, setUpdated] = useState(false);
+  let { id } = useParams();
 
-    useEffect(() => {
-        const grabTestimonial = async () => {
-            const testimonial = await getTestimonial(id)
-            setTestimonial(testimonial)
-        }
-        grabTestimonial()
-    }, [id]);
-
-    const handleChange = (e) => {
-        const { name, value } = e.target
-        setTestimonial({
-            ...testimonial, 
-            [name]: value
-        });
+  useEffect(() => {
+    const grabTestimonial = async () => {
+      const testimonial = await getTestimonial(id);
+      setTestimonial(testimonial);
     };
+    grabTestimonial();
+  }, [id]);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        const updateReq = await updateTestimonial(id, testimonial)
-        setUpdated(true) 
-    }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTestimonial({
+      ...testimonial,
+      [name]: value,
+    });
+  };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const updateReq = await updateTestimonial(id, testimonial);
+    setUpdated(true);
+  };
 
-     if (updated){
-        return <Redirect to={"/our-stories"}/>
-    } 
-    return (
-        <Layout>
-        <div className="complete-testimonial-detail-screen">
-            <h1 className="edit-header">Edit Your Testimonial</h1>
-            <div className="testimonial-form">
-                <form onSubmit={handleSubmit}>
-
-                    <textarea 
-                        className="edit-content"
-                        name="content"
-                        value={testimonial.content}
-                        onChange={handleChange}
-                        required
-                    />
-                    <input 
-                        className="edit-author"
-                        name="author"
-                        value={testimonial.author}
-                        onChange={handleChange}
-                        required
-                    />
-                    <div className="button-container">
-                        <button type='submit' className="button-save">Save</button>
-                        <button 
-                            className="button-delete"
-                            onClick={()=> deleteTestimonial(testimonial._id)}
-                        >Delete</button>
-                    </div>
-                </form>
+  if (updated) {
+    return <Redirect to={"/our-stories"} />;
+  }
+  return (
+    <Layout>
+      <div className="complete-testimonial-detail-screen">
+        <h1 className="edit-header">Edit Your Testimonial</h1>
+        <div className="testimonial-form">
+          <form onSubmit={handleSubmit}>
+            <img src={LeftQuote} alt="quote mark" className="edit-left-quote" />
+            <textarea
+              className="edit-content"
+              name="content"
+              value={testimonial.content}
+              onChange={handleChange}
+              required
+            />
+            <input
+              className="edit-author"
+              name="author"
+              value={testimonial.author}
+              onChange={handleChange}
+              required
+            />
+            <img
+              src={RightQuote}
+              alt="quote mark"
+              className="edit-right-quote"
+            />
+            <div className="button-container">
+              <button type="submit" className="button-save">
+                Save
+              </button>
+              <button
+                className="button-delete"
+                onClick={() => deleteTestimonial(testimonial._id)}
+              >
+                Delete
+              </button>
             </div>
-
+          </form>
         </div>
-        </Layout>
-    )
-}
+      </div>
+    </Layout>
+  );
+};
 
 export default TestimonialDetail;
