d4cddab55ec213b665b659a729b58884376d238c
donnywarren9 <donnywarren9@gmail.com>
2020-08-27 19:54:07 -0400

    nav bar styling

--- client/src/Components/Shared/Nav/Nav.jsx
+++ client/src/Components/Shared/Nav/Nav.jsx
@@ -1,44 +1,11 @@
-import React, { Component, useState } from "react";
-import {
-  Dropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-} from "reactstrap";
+import React from "react";
+
 import { NavLink } from "react-router-dom";
-// import 'react-dropdown/style.css'
 import "./Nav.css";
 import Social from "../../Social/Social";
 import Logo from "../../../Assets/new-alternatives-title.png";
 
-const Nav = (props) => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-
-  const toggle = () => setDropdownOpen((prevState) => !prevState);
-
-  // export class Nav extends React.Component {
-  //   constructor(props) {
-  //     super(props)
-
-  //     this.toggle = this.toggle.bind(this)
-  //     this.onMouseEnter = this.onMouseEnter.bind(this)
-  //     this.onMouseLeave = this.onMouseLeave.bind(this)
-  //     this.state = {
-  //       dropdownOpen: false
-  //     }
-  //   }
-  //   toggle() {
-  //     this.setState(prevState => ({
-  //       dropdownOpen: !prevState.dropdownOpen
-  //     }))
-  //   }
-  //   onMouseEnter() {
-  //     this.setState({dropdownOpen: true})
-  //   }
-  //   onMouseLeave() {
-  //     this.setState({dropdownOpen: false})
-  //   }
-  //   render() {
+const Nav = () => {
   return (
     <>
       <div className="nav-component">
@@ -52,12 +19,13 @@ const Nav = (props) => {
         <div className="nav-bar">
           <div className="top-nav-bar">
             <div>
-              <NavLink className="nav-links" to="/coming-soon">
-                <h6 className="contact-us">Contact Us</h6>
+              <NavLink className="contact-us nav-links" to="/coming-soon">
+                Contact Us
               </NavLink>
             </div>
-
-            <Social className="social-media" />
+            <div className="social-media-box">
+              <Social />
+            </div>
             <button className="donate" alt="donate button">
               <a
                 href="https://fundraise.newalternativesnyc.org/give/157653/#!/donation/checkout"
@@ -67,6 +35,7 @@ const Nav = (props) => {
               </a>
             </button>
           </div>
+
           <div className="nav-menu" alt="navigation menu">
             <NavLink className="nav-links" to="/">
               Home
@@ -81,38 +50,13 @@ const Nav = (props) => {
             </NavLink>
 
             <NavLink className="nav-links" to="/coming-soon">
-              {/* <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-                  <DropdownToggle caret> */}
               Get Help
-              {/* </DropdownToggle> */}
-              {/* <DropdownMenu className="get-help-menu">
-                    <DropdownItem className="dropdown-item">Registration + Schedule</DropdownItem>
-                    <DropdownItem divider />
-                    <DropdownItem className="dropdown-item">Support Programs</DropdownItem>
-                    <DropdownItem divider />
-                    <DropdownItem className="dropdown-item">Client Resources</DropdownItem>
-                  </DropdownMenu>
-                </Dropdown> */}
             </NavLink>
 
             <NavLink className="nav-links" to="/coming-soon">
-              {/* <Dropdown className='get-involved' onMouseOver={this.onMouseEnter} onMouseLeave={this.onMouseLeave} isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-                  <DropdownToggle caret> */}
               Get Involved
-              {/* </DropdownToggle>
-                  <DropdownMenu className="get-involved-menu">
-                    <DropdownItem className="dropdown-item">Volunteer</DropdownItem>
-                    <DropdownItem divider />
-                    <DropdownItem className="dropdown-item">Fundraise</DropdownItem>
-                    <DropdownItem divider />
-                    <DropdownItem className="dropdown-item">Upcoming Events</DropdownItem>
-                  </DropdownMenu>
-                </Dropdown> */}
             </NavLink>
           </div>
-          {/* <ul>
-              <h5>Contact Us</h5>
-            </ul> */}
         </div>
       </div>
     </>
