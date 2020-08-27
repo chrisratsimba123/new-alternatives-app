769fdfd85c04ec3677671027f5e27d75148c8392
Daynamarie <Dayna.christison@gmail.com>
2020-08-27 16:00:03 -0400

    formatted video component

--- client/src/Components/VideoCarousel/VideoCarousel.jsx
+++ client/src/Components/VideoCarousel/VideoCarousel.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import React from "react";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel";
 
-import 'pure-react-carousel/dist/react-carousel.es.css';
-import "./VideoCarousel.css"
+import "pure-react-carousel/dist/react-carousel.es.css";
+import "./VideoCarousel.css";
 import LeftArrow from "../../Assets/left-arrow.png";
 import RightArrow from "../../Assets/right-arrow.png";
 
@@ -13,79 +19,75 @@ import Red from "../../Assets/Videos/Red.mp4";
 import Yellow from "../../Assets/Videos/Yellow.mp4";
 
 const VideoCarousel = () => {
- 
+  return (
+    <div className="video-wrapper">
+      <CarouselProvider
+        className="carousel-complete"
+        naturalSlideWidth={400}
+        naturalSlideHeight={200}
+        totalSlides={5}
+        visibleSlides={3}
+        infinite={true}
+      >
+        <h3 className="video-header">#WhatAboutUs</h3>
+        <ButtonBack className="video-back-btn btn">
+          <img className="back-arrow" src={LeftArrow} />
+        </ButtonBack>
+        <Slider className="video-container">
+          <Slide>
+            <div className="c-video">
+              <video className="video" width="100%" height="100%" controls>
+                <source src={Blue} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          </Slide>
 
-    return(    
-        <div className="video-wrapper">                                             
-            <CarouselProvider
-                className="carousel-complete"
-                naturalSlideWidth={400}
-                naturalSlideHeight={200}
-                totalSlides={5}
-                visibleSlides={3}
-                infinite={true}
-            >
-                <h3 className="video-header">#WhatAboutUs</h3>
-                <ButtonBack className="video-back-btn btn">
-                    <img className="back-arrow" src={LeftArrow} />
-                </ButtonBack>          
-                <Slider className="video-container">
+          <Slide>
+            <div className="c-video">
+              <video className="video" width="100%" height="100%" controls>
+                <source src={Green} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          </Slide>
 
-                    <Slide>
-                        <div className="c-video">
-                            <video className="video" width="100%" height="100%" controls>
-                                <source src={Blue} type="video/mp4"/>
-                                Your browser does not support the video tag.
-                            </video>
-                        </div>
-                    </Slide>
-        
-                    <Slide>
-                        <div className="c-video">
-                            <video className="video" width="100%" height="100%" controls>
-                                <source src={Green} type="video/mp4"/>
-                                Your browser does not support the video tag.
-                            </video>
-                        </div>
-                    </Slide>
+          <Slide>
+            <div className="c-video">
+              <video className="video" width="100%" height="100%" controls>
+                <source src={Red} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          </Slide>
 
-                    <Slide>
-                        <div className="c-video">
-                        <video className="video" width="100%" height="100%" controls>
-                            <source src={Red} type="video/mp4"/>
-                            Your browser does not support the video tag.
-                        </video>
-                        </div>
-                    </Slide>
+          <Slide>
+            <div className="c-video">
+              <video className="video" width="100%" height="100%" controls>
+                <source src={Yellow} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          </Slide>
 
-                    <Slide>
-                        <div className="c-video">
-                        <video className="video" width="100%" height="100%" controls>
-                            <source src={Yellow} type="video/mp4"/>
-                            Your browser does not support the video tag.
-                        </video>
-                        </div>
-                    </Slide>
+          <Slide>
+            <div className="c-video">
+              <video className="video" width="100%" height="100%" controls>
+                <source src={Purple} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          </Slide>
+        </Slider>
+        <ButtonNext className="video-forward-btn btn">
+          <img className="forward-arrow" src={RightArrow} alt="right arrow" />
+        </ButtonNext>
+      </CarouselProvider>
+    </div>
+  );
+};
 
-                    <Slide>
-                        <div className="c-video">
-                        <video className="video" width="100%" height="100%" controls>
-                            <source src={Purple} type="video/mp4"/>
-                            Your browser does not support the video tag.
-                        </video>
-                        </div>
-                    </Slide>
-
-                </Slider>
-                <ButtonNext className="video-forward-btn btn">
-                    <img className="forward-arrow" src={RightArrow} alt="right arrow" />
-                </ButtonNext>
-            </CarouselProvider>
-        </div>
-    )
-}
-
-export default VideoCarousel
+export default VideoCarousel;
 
 /* CITED 
 //https://github.com/express-labs/pure-react-carousel 
